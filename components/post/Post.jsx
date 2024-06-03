06058075b0674e852d4eaf27fec4e51596396c79
Sakai-Kaede <kana1024k@gmail.com>
2024-06-03 22:58:38 +0900

    Update Post.jsx

--- components/post/Post.jsx
+++ components/post/Post.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Post.css'
 import { MoreVert } from '@mui/icons-material'
-import { Users } from "../../dummyData"
+import axios from 'axios'
+// import { Users } from "../../dummyData"
 
 export default function Post({post}) {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
@@ -14,13 +15,24 @@ export default function Post({post}) {
     setIsLiked(!isLiked);
   }
 
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    // 無名関数にasyncはつけられない
+    const fetchUser = async() => {
+      const response = await axios.get(`/users/${post.userId}`);
+      console.log(response);
+      setUser(response.data);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <div className='post'>
       <div className='postWrapper'>
         <div className='postTop'>
           <div className='postTopLeft'>
-            <img src={ PUBLIC_FOLDER + Users.filter((user) => user.id === post.id)[0].profilePicture } alt="" className='postProfileImg' />
-            <span className='postUsername'>{ Users.filter((user) => user.id === post.id)[0].username }</span>
+            <img src={ user.profilePicture } alt="" className='postProfileImg' />
+            <span className='postUsername'>{ user.username }</span>
             <span className='postDate'>{post.date}</span>
           </div>
           <div className='postTopRight'>
