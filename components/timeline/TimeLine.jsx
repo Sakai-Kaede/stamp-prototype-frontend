be7f9aeef734a99e974110c9378e90c6553b2e29
Sakai-Kaede <kana1024k@gmail.com>
2024-06-07 22:10:48 +0900

    Update TimeLine.jsx

--- components/timeline/TimeLine.jsx
+++ components/timeline/TimeLine.jsx
@@ -5,27 +5,28 @@ import Post from '../post/Post'
 import axios from "axios"
 // import {Posts} from "../../dummyData"
 
-export default function TimeLine() {
+export default function TimeLine({ username }) {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     // 無名関数にasyncはつけられない
     const fetchPosts = async() => {
-      const response = await axios.get("/posts/timeline/6642c21b9fe01f5810fcec38");
+      const response = username 
+        ? await axios.get(`/posts/profile/${username}`) 
+        : await axios.get("/posts/timeline/6642c21b9fe01f5810fcec38");
       // console.log(response);
       setPosts(response.data);
     };
     fetchPosts();
-  }, [])
+  }, []);
 
   return (
     <div className='timeline'>
       <div className='timelineWrapper'>
         <Share />
         {posts.map((post) => (
-          <Post post={post} key={post.id} />
+          <Post post={post} key={post._id} />
         ))}
       </div>
     </div>
   )
 }
-
