a33edf10d644cdbc5cc9458c7ab423eacba5656c
Sakai-Kaede <kana1024k@gmail.com>
2024-06-08 20:02:44 +0900

    First commit AuthContext.js

---
+++ state/AuthContext.js
@@ -0,0 +1,24 @@
+import {createContext, useReducer} from 'react'
+import AuthReducer from './AuthReducer';
+
+// 最初のユーザ状態を定義
+const initialState = {
+  user: null,
+  isFetching: false,
+  error: false,
+};
+
+// 状態をグローバルに管理する
+export const AuthContext = createContext(initialState);
+
+export const AuthContextProvider = ({children}) => {
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
+  return <AuthContext.Provider value={{
+    user: state.user,
+    isFetching: state.isFetching,
+    error: state.error,
+    dispatch,
+  }}>
+    {children}
+  </AuthContext.Provider>
+}
